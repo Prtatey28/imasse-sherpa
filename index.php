8e40fd00ea7a322ce1b5c1b815e506f05d4c5657
Preston Tateyama <prtatey28@gmail.com>
2023-09-02 10:49:08 -0600

    remove test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <head>
-  <title>Sherpa - Pathway Home - Test</title>
+  <title>Sherpa - Pathway Home</title>
 </head>
 <body style="display: none">
 <style>
