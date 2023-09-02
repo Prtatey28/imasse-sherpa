f4df4389ec818fe715a2d0bf23a07448b1f1f41a
Preston Tateyama <prtatey28@gmail.com>
2023-09-02 16:25:20 -0600

    Comments were added to help understand the code

--- index.php
+++ index.php
@@ -1,41 +1,50 @@
+<!--Welcome to the code for the website, anything in green are comments. These will help explain what's going on-->
+<!--This piece of code makes the entire code doc in HTML format-->
 <!DOCTYPE html>
+<!--Signifies start of HTML code-->
 <html>
+<!--This is the meta data for the website. It explains the scale and the ratios of the website-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<!--This is the "tab" name of the website. It will display in all of your tabs of your browser-->
 <head>
   <title>Sherpa - Pathway Home</title>
 </head>
+
+<!--This defines the body element, which includes every single element on the web page-->
 <body style="display: none">
 
-<!--This is the start of the CSS for designing the website first page-->
+<!--This is the start of the CSS for designing the website first page HTML uses CSS to understand how to "decorate" things with colors and sizes-->
 <style>
     * {
   box-sizing: border-box;
 }
-
+/*CSS for whole website page*/
 body {
   font: 16px Arial;  
   background-color: #6e6b6a;
 }
+/*This is the CSS for the live search box of the website*/
 /*the container must be positioned relative:*/
 .autocomplete {
   position: relative;
   display: inline-block;
       width: 80%;
 }
-
+/*This is the CSS for the search box, it adjusts how big it is and the kind of text being inputted*/
 input {
   border: 1px solid transparent;
-  background-color: #00ff00;
   padding: 10px;
   font-size: 16px;
 }
 
+/* This is also CSS for the search box. It adjusts color and size of it*/
 input[type=text] {
   background-color: #f1f1f1;
   width: 100%;
 }
 
+/*This is the CSS for the search button of the webpage*/
 input[type=submit] {
   background-color: DodgerBlue;
   color: #fff;
@@ -43,7 +52,7 @@ input[type=submit] {
   width: 19%;
 }
 
-
+/*This is the CSS for classes that are populated below when you type in certain letters or numbers*/
 .autocomplete-items {
    position: absolute;
   border: 1px solid #d4d4d4;
@@ -56,6 +65,7 @@ input[type=submit] {
   right: 0;
 }
 
+/* These are additional properties for the populating classes below search bar as you type them in*/
 .autocomplete-items div {
   padding: 10px;
   cursor: pointer;
@@ -63,12 +73,12 @@ input[type=submit] {
   border-bottom: 1px solid #d4d4d4; 
 }
 
-/*when hovering an item:*/
+/*when hovering an item: This will give the color it changes to from white*/
 .autocomplete-items div:hover {
   background-color: #e9e9e9; 
 }
 
-/*when navigating through the items using the arrow keys:*/
+/*when navigating through the items using the arrow keys: This will change the color of the highlighted portion*/
 .autocomplete-active {
   background-color: DodgerBlue !important; 
   color: #ffffff; 
@@ -76,6 +86,7 @@ input[type=submit] {
 #hidden {
     display: none;
 }
+/*adjusts position for search bar, populated search results, logo. Also adjusts logo size */
 .autocomplete {
     position: relative;
   display: inline-block;
@@ -92,25 +103,33 @@ input[type=submit] {
     width: 300px;
 }
 </style>
+<!--This is the end of the CSS portion of the Code -->
+
+<!--This piece of the code is all about making everything show up on the website. This is typical HTML-->
+<!--Title Header-->
 <h1>
 <p style="text-align:center">Welcome to Sherpa! Input Classes Below!</p>
 </h1>
 
+<!--This piece of the code places all of the elements on the website, based on their CSS location, size, and color-->
 <div class="container">
     <div class='logo'>
 <img src="img/sherpa-logo.svg">
 </div>
+<!--This code is what is run when the search button is pressed. The search bar is turned off, and it sends you to the next webpage, search.php-->
 <form autocomplete="off" id="form" action="/search.php" method="post">
     <div class="autocomplete">
+  <!--This code is used to define what's inside the search bar-->
   <input id="input" type="text" placeholder="Start Typing Class Name" name="input">
   </div>
   <input type="submit" value="Search" name="submit">
 </form>
+<!--This code determines where the title "Classes entered" is located below the search bar-->
 <p style="background-color:#fff;text-align:center">Classes Entered</p>
 <ul id="classes"></ul>
 </div>
 
-
+<!--This is the JavaScript portion of the code. This is how the populating search bar works and and how selected classes are printed below-->
 <script>
 var counter = 0;
 let classesEntered = [];
@@ -202,6 +221,7 @@ function autocomplete(inp, arr) {
   });
 }
 
+//Sets the variable "data" equal to a string of this entire JSON file
 var data = `
 [
     {
@@ -274,6 +294,7 @@ var data = `
     }
   ]`;
 
+//This creates a javascript object for the entire JSON string of characters above
 var classes = JSON.parse(data);
 
 autocomplete(document.getElementById("input"), classes);
