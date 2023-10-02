884de0f9fd77fde9b99b7876016cbc9e3656af68
Preston Tateyama <prtatey28@gmail.com>
2023-10-01 20:28:01 -0600

    created secondary google sheet to house all possible classes that can be selected from search bar

--- index.php
+++ index.php
@@ -157,13 +157,63 @@ for ($j = 0; $j < $count; $j++) {
 // Print it out as JSON
 echo json_encode($newArray);
 
-
-$writeJson = file_put_contents("json/A1-P1.json", json_encode($newArray));
 // writeJson adapted from: https://stackoverflow.com/questions/57731341/how-to-push-a-new-object-into-a-json-file-using-php
-// Push $newArray into JSON file COOLIO
-//for ($x = 0; $x < count($newArray); $x++){
-  //$writeJson = file_put_contents("COOLIO2.json", json_encode(json_decode(implode($newArray[0])),JSON_PRETTY_PRINT));
-//}
+$writeJson = file_put_contents("json/A1-P1.json", json_encode($newArray));
+
+
+
+//new array grabber to grab all possible classes.
+//array grabber adapted from: https://www.ravelrumba.com/blog/json-google-spreadsheets/
+$feed = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQowsFTPvKhT-1F2bg8yHsGZLWMW924YhMZbFrWOP-AdpEiXsx9ywsGYj6aDJhM57xDO8caEeflDzat/pub?output=csv';
+$keys = array();
+$newArray = array();
+
+function csvToArray2($file, $delimiter){
+  if (($handle = fopen($file, 'r')) !== FALSE){
+    $i = 0;
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
+      for ($j = 0; $j < count($lineArray); $j++){
+        $arr[$i][$j] = $lineArray[$j];
+      }
+      $i++;
+    }
+    fclose($handle);
+  }
+  return $arr;
+}
+
+// Do it
+$data = csvToArray2($feed, ',');
+ 
+// Set number of elements (minus 1 because we shift off the first row)
+$count = count($data) - 1;
+ 
+//Use first row for names  
+$labels = array_shift($data);  
+ 
+foreach ($labels as $label) {
+  $keys[] = $label;
+}
+ 
+// Add Ids, just in case we want them later
+$keys[] = 'id';
+ 
+for ($i = 0; $i < $count; $i++) {
+  $data[$i][] = $i;
+}
+ 
+// Bring it all together
+for ($j = 0; $j < $count; $j++) {
+  $d = array_combine($keys, $data[$j]);
+  
+  $newArray[$j] = $d;
+}
+ 
+// Print it out as JSON
+echo json_encode($newArray);
+
+// writeJson adapted from: https://stackoverflow.com/questions/57731341/how-to-push-a-new-object-into-a-json-file-using-php
+$writeJson = file_put_contents("json/allClasses.json", json_encode($newArray));
 
 ?>
 <h1>
