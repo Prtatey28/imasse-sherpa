a02c900c9ab24cffe20737f48151cdd964d9ffa8
Preston Tateyama <prtatey28@gmail.com>
2023-10-18 13:01:04 -0600

    set up classPull.php to grab classes from google sheets and parse into json file separately from doing it on a index.php. This will allow website to run much quicker and free-er. Also, this gets rid of extra errors.

--- index.php
+++ index.php
@@ -107,136 +107,6 @@ input[type=submit] {
 
 <!--This piece of the code is all about making everything show up on the website. This is typical HTML-->
 <!--Title Header-->
-<?php
-
-
-//function for pulling classes from google sheet
-function csvToArray($file, $delimiter){
-  if (($handle = fopen($file, 'r')) !== FALSE){
-    $i = 0;
-    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
-      for ($j = 0; $j < count($lineArray); $j++){
-        $arr[$i][$j] = $lineArray[$j];
-      }
-      $i++;
-    }
-    fclose($handle);
-  }
-  return $arr;
-}
-
-
-
-
-
-
-
-$file2 = file_get_contents("json/sheetUrls.json");
-$file2 = json_decode($file2, true);
-
-foreach($file2 as $yy){
-foreach($yy[0]['url'] as $zz){
-  
-//main issue at the moment. Can't seem to pass url from json to $feed variable below successfully
-
-
-//array grabber adapted from: https://www.ravelrumba.com/blog/json-google-spreadsheets/
-
-$feed = $zz;
-$keys = array();
-$newArray = array();
-
-//function for csvToArray was moved above foreach loop
-
-// Do it
-$data = csvToArray($feed, ',');
- 
-// Set number of elements (minus 1 because we shift off the first row)
-$count = count($data) - 1;
- 
-//Use first row for names  
-$labels = array_shift($data);  
- 
-foreach ($labels as $label) {
-  $keys[] = $label;
-}
- 
-// Add Ids, just in case we want them later
-$keys[] = 'id';
- 
-for ($i = 0; $i < $count; $i++) {
-  $data[$i][] = $i;
-}
- 
-// Bring it all together
-for ($j = 0; $j < $count; $j++) {
-  $d = array_combine($keys, $data[$j]);
-  
-  $newArray[$j] = $d;
-}
- 
-// Print it out as JSON
-//echo json_encode($newArray);
-// writeJson adapted from: https://stackoverflow.com/questions/57731341/how-to-push-a-new-object-into-a-json-file-using-php
-$writeJson = file_put_contents('json/'. $yy[0]['jsonName'] .'.json', json_encode($newArray));
-}
-}
-
-
-//new array grabber to grab all possible classes.
-//array grabber adapted from: https://www.ravelrumba.com/blog/json-google-spreadsheets/
-$feed = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQowsFTPvKhT-1F2bg8yHsGZLWMW924YhMZbFrWOP-AdpEiXsx9ywsGYj6aDJhM57xDO8caEeflDzat/pub?output=csv';
-$keys = array();
-$newArray = array();
-
-function csvToArray2($file, $delimiter){
-  if (($handle = fopen($file, 'r')) !== FALSE){
-    $i = 0;
-    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
-      for ($j = 0; $j < count($lineArray); $j++){
-        $arr[$i][$j] = $lineArray[$j];
-      }
-      $i++;
-    }
-    fclose($handle);
-  }
-  return $arr;
-}
-
-// Do it
-$data = csvToArray2($feed, ',');
- 
-// Set number of elements (minus 1 because we shift off the first row)
-$count = count($data) - 1;
- 
-//Use first row for names  
-$labels = array_shift($data);  
- 
-foreach ($labels as $label) {
-  $keys[] = $label;
-}
- 
-// Add Ids, just in case we want them later
-/*$keys[] = 'id';
- 
-for ($i = 0; $i < $count; $i++) {
-  $data[$i][] = $i;
-}*/
- 
-// Bring it all together
-for ($j = 0; $j < $count; $j++) {
-  $d = array_combine($keys, $data[$j]);
-  
-  $newArray[$j] = $d;
-}
- 
-// Print it out as JSON
-//echo json_encode($newArray);
-
-// writeJson adapted from: https://stackoverflow.com/questions/57731341/how-to-push-a-new-object-into-a-json-file-using-php
-$writeJson = file_put_contents("json/allClasses.json", json_encode($newArray));
-
-?>
 <h1>
 <p style="text-align:center"> WELCOME TO SHERPA!</p>
 </h1>
@@ -263,7 +133,8 @@ $writeJson = file_put_contents("json/allClasses.json", json_encode($newArray));
 <script>
 //grabs the php array from above and uses it here for the possible classes search bar
 //echo part of code adapted from https://www.geeksforgeeks.org/how-to-pass-a-php-array-to-a-javascript-function/#
-var data2 = <?php echo json_encode($newArray);?>;
+
+var data2 = <?php echo file_get_contents('json/allClasses.json');?>;
 //console.log(data2);
 
 
