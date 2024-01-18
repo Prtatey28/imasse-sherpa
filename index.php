13958dddcceb85f347175be8ee03c32706ad8586
Preston Tateyama <prtatey28@gmail.com>
2024-01-18 00:27:37 -0700

    made instructions easier to read

--- index.php
+++ index.php
@@ -130,6 +130,12 @@
     .removeAllButton:hover {
       color: red;
     }
+    .list-1{
+      padding: 5px;
+    }
+    .list-1-li{
+      margin-bottom: 10px;
+    }
   </style>
   <!--This is the end of the CSS portion of the Code -->
   <!--This piece of the code is all about making everything show up on the website. This is typical HTML-->
@@ -142,9 +148,12 @@
       <a href="https://sites.google.com/jeffcoschools.us/prestontateyama-pathwaypre-che/home" target="_blank"><img src="img/Sherpa_Logo.png"></a>
     </div>
     <p style="position: bottom; text-align:center; font-size:20px;"><mark>Enter Your Classes Below, <b>BY SEMESTER,</b> and Press the Blue "Check" Button When You Are Ready to Continue</mark></p>
-    <p style="position: bottom; text-align:center; font-size:18px;"><b><u>Keep In Mind:</u></b> <ul><li>Multiple year classes are subjugated by "I" not "#." (Ex: Algebra 2 = Algebra II.)</li> 
-      <li>Math 4th year is its own class, make sure to add it if it applies to you!</li>
-      <li>Geometry in construction and AMPED are their own classes (Ex: Add AMPED, not AMPED + Algebra I)</li>
+    <p style="position: bottom; text-align:center; font-size:18px; background-color:pink;"><b><u>Keep In Mind:</u></b> 
+    <ul class="list-1">
+      <li class="list-1-li"><mark>You are adding every single <b>PASSED</b> class from your highschool career so far, including the classes you are taking this semester</mark></li>
+      <li class="list-1-li">Multiple year classes are subjugated by "I" not "#." (Ex: Algebra 2 = Algebra II.)</li> 
+      <li class="list-1-li">Math 4th year is its own class, make sure to add it if it applies to you!</li>
+      <li class="list-1-li">Geometry in construction and AMPED are their own classes (Ex: Add AMPED, not AMPED + Algebra I)</li>
     </ul></p>
     <!--This code is what is run when the search button is pressed. The search bar is turned off, and it sends you to the next webpage, search.php-->
     <form autocomplete="off" id="form" action="/search.php" method="post">
