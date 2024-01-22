de082c8b21fc183851014d150b4a667974eabb16
Preston Tateyama <prtatey28@gmail.com>
2024-01-22 13:30:17 -0700

    updated highlight on IDS search

--- search.php
+++ search.php
@@ -251,7 +251,7 @@ $file = json_decode($file, true);
   }
   foreach ($file as $y) {
   ?>
-    <p style="font-family: Arial; background-color:yellow; color:black; font-size: 25px">Not sure what IDS is? Click <a href='https://docs.google.com/document/d/1Sb5T9UpqaVv87lefkwTaRzjJZBBoecEDOivG_zqZh9k/edit?usp=sharing' target="_blank"><b>HERE</b></a> to check wiki. Scroll to the bottom of the FRQ and find highlighted portion. </mark></p>
+    <p style="font-family: Arial; color:black; font-size: 25px"><mark>Not sure what IDS is? Click <a href='https://docs.google.com/document/d/1Sb5T9UpqaVv87lefkwTaRzjJZBBoecEDOivG_zqZh9k/edit?usp=sharing' target="_blank"><b>HERE</b></a> to check wiki. Scroll to the bottom of the FRQ and find highlighted portion. </mark></p>
     <h5 style="font-family: Arial"><mark> Do I Qualify for IDS? </mark></h5>
     <h4 style="color: <?= $checkColor2 ?>"><b> <?php echo $checkPrint2 ?> </b> </h4>
     <p style="font-size: 25px"><mark> <b>NOTE:</b> While it may say you've completed IDS below, you, yourself, still need to verify the requirement above certain Academies to ensure that you still qualify! </mark></p>
